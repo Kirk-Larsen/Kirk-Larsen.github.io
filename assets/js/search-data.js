4bb4f77515266bf10519b3d7568163d7c9e9afd9
Kirk-Larsen <kirk.a.larsen1@gmail.com>
2025-04-29 06:13:04 +0000

    Deploying to gh-pages from @ Kirk-Larsen/Kirk-Larsen.github.io@f11f9d0b7607436a2a37885bd89bd340735c2de8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -69,11 +69,6 @@ ninja.data = [{
           description: "Advancing time-resolved hard x-ray scattering experiments via spectral broadening and post-compression",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
